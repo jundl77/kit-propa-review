a7493893297215c03a01413f71c38e73eeb72c02
Julian <julianbrendl@gmail.com>
2018-08-09 22:01:12 +0200

    added pdf

--- build.js
+++ build.js
@@ -3,4 +3,4 @@ const fs = require("fs");
 
 fs.createReadStream("src/index.md")
   .pipe(markdownPDF())
-  .pipe(fs.createWriteStream("build/review.pdf"))
+  .pipe(fs.createWriteStream("pdf/review.pdf"))
